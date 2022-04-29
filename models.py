9b6855a7ebc61092db3bf0ca353a2c1aa6150082
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-30 00:19:59 +0500

    Create some pydantic models

---
+++ models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, validator
+from enum import Enum
+
+
+class CourierTypeEnum(str, Enum):
+    foot = "foot"
+    bike = "bike"
+    car = "car"
+
+
+class CourierItem(BaseModel):
+    courier_id: int
+    courier_type: CourierTypeEnum
+    regions: list[int]
+    working_hours: list[str]
+
+
+class CouriersPostRequest(BaseModel):
+    data: list[CourierItem]
