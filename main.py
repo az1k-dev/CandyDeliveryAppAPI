f2c0503ddf34b3394898948a49805f93a9feb142
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-30 00:25:18 +0500

    Add controller for GET "/couriers/{courier_id}"

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.post("/couriers")
 def import_couriers(couriers: models.CouriersPostRequest):
     return couriers
+
+
+@app.get("/couriers/{courier_id}")
+def import_couriers(courier_id: int):
+    return f"Hey, courier {courier_id}"
