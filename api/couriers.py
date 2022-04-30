2cd5620bcf37b229bfe4981fb0ac90821f9ed374
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-30 23:41:51 +0500

    Add courier service into api controllers

--- api/couriers.py
+++ api/couriers.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, status, Depends
-from sqlalchemy.orm import Session
-import models.couriers
-from database import get_session
+from services.couriers import CouriersService
+from models.couriers import CouriersPostRequest
 
 router = APIRouter(prefix="/couriers")
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def import_couriers(couriers: models.couriers.CouriersPostRequest, session: Session = Depends(get_session)):
-    return couriers
+def import_couriers(couriers: CouriersPostRequest, service: CouriersService = Depends()):
+    return service.import_couriers(couriers)
 
 
 @router.get("/{courier_id}")
-def get_courier_info(courier_id: int):
-    return f"Hey, courier {courier_id}"
+def get_courier_info(courier_id: int, service: CouriersService = Depends()):
+    return service.get_courier(courier_id)
