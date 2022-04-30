142baf7343be1915c9967b859d189c3e6725dc94
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-30 21:29:57 +0500

    Add hours and orders-hours, couriers-hours relationships

--- tables.py
+++ tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, DateTime
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -11,20 +11,37 @@ couriers_regions = Table(
 )
 
 
+orders_hours = Table(
+    "orders_hours", Base.metadata,
+    Column("order_id", ForeignKey("orders.id")),
+    Column("hour_id", ForeignKey("hours.id"))
+)
+
+
+couriers_hours = Table(
+    "couriers_hours", Base.metadata,
+    Column("courier_id", ForeignKey("couriers.id")),
+    Column("hour_id", ForeignKey("hours.id"))
+)
+
+
 class Courier(Base):
     __tablename__ = "couriers"
 
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     courier_type = Column(String)
+
     regions = relationship("Region", secondary=couriers_regions, back_populates="couriers")
+    hours = relationship("Hour", secondary=couriers_hours, back_populates="couriers")
     orders = relationship("Order", back_populates="courier")
 
 
 class Region(Base):
     __tablename__ = "regions"
 
-    id = Column(Integer, primary_key=True, unique=True)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     region = Column(Integer, nullable=False, unique=True)
+
     couriers = relationship("Courier", secondary=couriers_regions, back_populates="regions")
     orders = relationship("Order", back_populates="region")
 
@@ -34,8 +51,21 @@ class Order(Base):
 
     id = Column(Integer, primary_key=True, unique=True, autoincrement=False)
     weight = Column(Integer)
+    assign_time = Column(DateTime)
+    complete_time = Column(DateTime)
     courier_id = Column(Integer, ForeignKey("couriers.id"))
     region_id = Column(Integer, ForeignKey("regions.id"))
 
     courier = relationship("Courier", back_populates="orders")
     region = relationship("Region", back_populates="orders")
+    hours = relationship("Hour", secondary=orders_hours, back_populates="orders")
+
+
+class Hour(Base):
+    __tablename__ = "hours"
+
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    hour = Column(String, unique=True)
+
+    couriers = relationship("Courier", secondary=couriers_hours, back_populates="hours")
+    orders = relationship("Order", secondary=orders_hours, back_populates="hours")
