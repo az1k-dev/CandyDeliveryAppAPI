e9845b9f5be1c00c6ccf82915faf5d803c8537b2
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-05-01 00:23:41 +0500

    Add new models for couriers

--- models/couriers.py
+++ models/couriers.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, validator, Field
 from enum import Enum
+from models.regions import RegionItem
+from models.hours import HourItem
 
 
 class CourierTypeEnum(str, Enum):
@@ -9,14 +11,37 @@ class CourierTypeEnum(str, Enum):
 
 
 class CourierItem(BaseModel):
-    id: int = Field(alias="courier_id")
+    courier_id: int
     courier_type: CourierTypeEnum
-    regions: list[int]
-    working_hours: list[str]
+    regions: list[RegionItem]
+    working_hours: list[HourItem]
 
     class Config:
         orm_mode = True
 
+    def get_getresponse(self):
+        return CourierGetResponse(
+            courier_id=self.courier_id,
+            courier_type=self.courier_type,
+            regions=list(map(lambda r: r.region, self.regions)),
+            working_hours=list(map(lambda h: h.hour, self.working_hours))
+        )
+
+
+class CourierPostRequest(BaseModel):
+    id: int
+    courier_type = CourierTypeEnum
+    regions: list[int]
+    working_hours: list[str]
+
 
 class CouriersPostRequest(BaseModel):
-    data: list[CourierItem]
+    data: list[CourierPostRequest]
+
+
+class CourierGetResponse(BaseModel):
+    courier_id: int
+    courier_type: str
+
+    regions: list[int]
+    working_hours: list[str]
